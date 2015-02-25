a14bedff79e400187a7fccd0b6602fe086e07c33
roland stumpner <roland@pcmgmt228>
2015-02-25 19:24:40 +0100

    get a list events with tire

--- app/app.rb
+++ app/app.rb
@@ -78,10 +78,10 @@ get '/eventsshowall' do
   #esearch = Elasticsearch::Client.new log: true
   #@events = es.search index: 'nibbs'
 
-  es = Tire.search 'nibbs' do
-      query do
-        string 'title:*'
-      end
+  #es = Tire.search 'nibbs' do
+  #    query do
+  #      string 'title:*'
+  #    end
 
 
   #haml :eventsshowall
